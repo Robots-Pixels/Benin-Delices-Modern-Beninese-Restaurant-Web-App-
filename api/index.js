c602b5b4ddd3c91a897ce92f9491a92eb3686146
Robots-Pixels <robotspixels@gmail.com>
2025-01-01 18:10:46 +0100

    Création de l'API et test

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get("/api/", (req, res) => {
+    res.json({ message: "Hello from server!" });
 })
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
+
+app.use(express.json());
+
--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,1 +1,34 @@
 import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    nom:{
+        type: String,
+        required: true
+    },
+
+    prenom:{
+        type: String,
+        required: false
+    },
+
+    password:{
+        type: String,
+        required: true
+    },
+
+    email:{
+        type: String,
+        required: false
+    },
+    
+    telephone:{
+        type: String,
+        required: true
+    }
+}, 
+    {
+        timestamps: true
+    }
+)
+
+export const User = mongoose.model("User", userSchema);
