13a509e976a1563c086c3d3849073dd3e271f4ae
Robots-Pixels <robotspixels@gmail.com>
2025-01-01 18:24:34 +0100

    Se connecter à MongoDB

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
 import express from "express";
 const app = express();
+import mongoose from "mongoose";
+
+import dotenv from "dotenv"
+dotenv.config();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    })
+})
 
 app.get("/api/", (req, res) => {
     res.json({ message: "Hello from server!" });
 })
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
-
-app.use(express.json());
-
